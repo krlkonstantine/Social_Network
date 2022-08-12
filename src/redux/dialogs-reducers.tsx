3d62ae834bb68d9cc0ceb2af3f8936568716efaa
krlkonstantine <kostea.023@gmail.com>
2022-08-12 09:22:57 +0300

    switch case added to reducers

--- src/redux/dialogs-reducers.tsx
+++ src/redux/dialogs-reducers.tsx
@@ -1,19 +1,22 @@
 import React, {ChangeEvent, useState} from 'react';
-import {ActionTypes, DialogsPageType, MessagesTextsType, StoreType} from "./state";
+import {ActionTypes, DialogsPageType, MessagesTextsType} from "./state";
 
- const sendNewMsg = 'SEND-NEW-MSG'
- const updNewMsgText = 'UPDATE-NEW-MSG-TEXT'
+const sendNewMsg = 'SEND-NEW-MSG'
+const updNewMsgText = 'UPDATE-NEW-MSG-TEXT'
 
-const dialogsReducer = (state:DialogsPageType,action:ActionTypes) => {
-    if (action.actionType === sendNewMsg) {
-        let newMessage: MessagesTextsType = {id: 4, messageText: state.newMessageText}
-        state.messages.push(newMessage)
-        state.newMessageText = ""
-    } else if (action.actionType === updNewMsgText) {
-        state.newMessageText = action.newMsgText
+const dialogsReducer = (state: DialogsPageType, action: ActionTypes) => {
+    switch (action.actionType) {
+        case sendNewMsg:
+            let newMessage: MessagesTextsType = {id: 4, messageText: state.newMessageText}
+            state.messages.push(newMessage)
+            state.newMessageText = ""
+            return state
+
+        case updNewMsgText:
+            state.newMessageText = action.newMsgText
+            return state
+        default:
+            return state
     }
-     return state
-
-
-};
- export default dialogsReducer
+}
+export default dialogsReducer
--- src/redux/profile-reducers.tsx
+++ src/redux/profile-reducers.tsx
@@ -1,19 +1,18 @@
 import React, {ChangeEvent, useState} from 'react';
 import {ActionTypes, DialogsPageType, PostsTextsType, ProfilePageType, StoreType} from "./state";
 
- const addNewPost = 'ADD-NEW-POST'
- const updNewPostText = 'UPDATE-NEW-POST-TEXT'
+const addNewPost = 'ADD-NEW-POST'
+const updNewPostText = 'UPDATE-NEW-POST-TEXT'
 
- const profileReducer = (state:ProfilePageType,action:ActionTypes) => {
+const profileReducer = (state: ProfilePageType, action: ActionTypes) => {
 
-        if (action.actionType === addNewPost) {
-            let newPost: PostsTextsType = {id: 4, messageText: state.newPostText, likeCount: '0'}
-            state.posts.push(newPost)
-            state.newPostText = ""
-        } else if (action.actionType === updNewPostText) {
-            state.newPostText = action.newPostText
-        }
-        return state
+    if (action.actionType === addNewPost) {
+        let newPost: PostsTextsType = {id: 4, messageText: state.newPostText, likeCount: '0'}
+        state.posts.push(newPost)
+        state.newPostText = ""
+    } else if (action.actionType === updNewPostText) {
+        state.newPostText = action.newPostText
+    }
+    return state
 }
-
 export default profileReducer
