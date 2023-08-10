7053dc467a63916e4b69525aa99f167397b1972c
krlkonstantine <krlkonstantine@gmail.com>
2023-08-10 13:26:31 +0300

    refactor: async await implemented in auth reducer + action type refactored into a constant

--- src/redux/reducers/auth-reducer.ts
+++ src/redux/reducers/auth-reducer.ts
@@ -12,6 +12,8 @@ export type AuthStateType = {
 }
 export type SetUserDataAT = ReturnType<typeof setAuthUserData>
 
+const SET_USER_DATA = 'social-network/auth/SET_USER_DATA'
+
 const initState: AuthStateType = {
     userId: null,
     email: null,
@@ -21,7 +23,7 @@ const initState: AuthStateType = {
 
 export const authReducer = (state = initState, action: ActionType): AuthStateType => {
     switch (action.type) {
-        case 'SET-USER-DATE':
+        case SET_USER_DATA:
             return {
                 ...state,
                 ...action.payload
@@ -32,47 +34,42 @@ export const authReducer = (state = initState, action: ActionType): AuthStateTyp
 }
 
 export const setAuthUserData = (userId: string | null, email: string | null, login: string | null, isAuth: boolean) => (
-    {type: 'SET-USER-DATE', payload: {userId, email, login, isAuth}} as const
+    {type: SET_USER_DATA, payload: {userId, email, login, isAuth}} as const
 )
 
 export const getAuthUser = () => {
-    return (dispatch: Dispatch<ActionType>) => {
+    return async (dispatch: Dispatch<ActionType>) => {
         dispatch(changePreloaderStatus(true))
-        return authAPI.me()
-            .then(data => {
-                if (data.resultCode === 0) {
-                    const {id, login, email} = data.data
-                    dispatch(setAuthUserData(id, email, login, true))
-                }
-                dispatch(changePreloaderStatus(false))
-            })
+        const res = await authAPI.me()
+        if (res.resultCode === 0) {
+            const {id, login, email} = res.data
+            dispatch(setAuthUserData(id, email, login, true))
+        }
+        dispatch(changePreloaderStatus(false))
+
     }
 }
 export const login = (email: string, password: string, rememberMe: boolean) => {
-    return (dispatch: Dispatch<ActionType>) => {
+    return async (dispatch: Dispatch<ActionType>) => {
         dispatch(changePreloaderStatus(true))
-        authAPI.login(email, password, rememberMe)
-            .then(data => {
-                if (data.resultCode === 0) {
-                    // @ts-ignore
-                    dispatch(getAuthUser())
-                } else {
-                    const errorMessage = data.messages.length > 0 ? data.messages[0] : 'Some error'
-                    dispatch(stopSubmit('login', {_error: errorMessage}))
-                }
-            })
-            .finally(() => dispatch(changePreloaderStatus(false)))
+        const res = await authAPI.login(email, password, rememberMe)
+        if (res.resultCode === 0) {
+            // @ts-ignore
+            dispatch(getAuthUser())
+        } else {
+            const errorMessage = res.messages.length > 0 ? res.messages[0] : 'Some error'
+            dispatch(stopSubmit('login', {_error: errorMessage}))
+        }
+        dispatch(changePreloaderStatus(false))
     }
 }
 export const logout = () => {
-    return (dispatch: Dispatch) => {
+    return async (dispatch: Dispatch) => {
         dispatch(changePreloaderStatus(true))
-        authAPI.logout()
-            .then(data => {
-                if (data.resultCode === 0) {
-                    dispatch(setAuthUserData(null, null, null, false))
-                }
-            })
-            .finally(() => dispatch(changePreloaderStatus(false)))
+        const res = await authAPI.logout()
+        if (res.resultCode === 0) {
+            dispatch(setAuthUserData(null, null, null, false))
+        }
+        dispatch(changePreloaderStatus(false))
     }
 }
