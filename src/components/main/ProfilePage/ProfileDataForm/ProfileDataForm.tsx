0cad1fc4afe13501f8d9b9cf55575941d489b2f4
krlkonstantine <krlkonstantine@gmail.com>
2023-08-16 10:46:47 +0300

    input fields and labels added. Need to set the submit function

--- src/components/main/ProfilePage/ProfileDataForm/ProfileDataForm.tsx
+++ src/components/main/ProfilePage/ProfileDataForm/ProfileDataForm.tsx
@@ -25,39 +25,28 @@ export const ProfileDataForm: React.FC<InjectedFormProps<ProfileFormData, Profil
     }
 
     return (
-        <form className={s.profileDataFormContainer}>
-            <button onClick={saveNewProfileInfo}>save</button>
-            <div className={s.profileItemInputContainer}>
-                <label className={s.itemLabel} style={{whiteSpace: "nowrap"}} htmlFor="fullName">
-                    Full name:
-                </label>
-                <Field
-                    className={s.itemInput}
-                    name="fullName"
-                    component={'input'}
-                />
-            </div>
-            
+        <form onSubmit={props.handleSubmit} className={s.profileDataFormContainer}>
+
+            <ProfileDataInput inputLabel={"Full name"} inputField={"fullName"}/>
             <ProfileDataInput inputLabel={"Open to opportunities"} inputField={"lookingForAJob"}
                               inputType={"checkbox"}/>
+            <ProfileDataInput inputLabel={"My skills"} inputField={"lookingForAJobDescription"}/>
             <ProfileDataInput inputLabel={"About me"} inputField={"aboutMe"}/>
-            <div><span>Full name</span>
+
+            <div className={s.contactsContainer}>
+                <span className={s.mainTitle}>Contacts:</span>
+                <ProfileDataInput inputLabel={"Tg"} inputField={"mainLink"}/>
+                <ProfileDataInput inputLabel={"Git"} inputField={"github"}/>
+                <ProfileDataInput inputLabel={"Vk"} inputField={"vk"}/>
+                <ProfileDataInput inputLabel={"Site"} inputField={"website"}/>
             </div>
-            <div><span>Обо мне:</span></div>
-            <div><span>Ищу работу:</span></div>
-            <div className={s.profile__descr}>My skills:</div>
-            <div>Обо мне:</div>
-            <div><span>Contacts:</span>
-                <div><span>Tg:</span></div>
-                <div><span>Git:</span></div>
-                <div><span>Fb:</span></div>
-                <div><span>Vk:</span></div>
-            </div>
+            <button className={s.submitButton} type="submit">Save Changes</button>
         </form>
     )
 }
 export const EditProfileDataReduxForm = reduxForm<ProfileFormData, ProfileFormProps>(
     {form: 'profile'})(ProfileDataForm)
+
 type ProfileDataInputType = {
     inputLabel: string
     inputField: string
